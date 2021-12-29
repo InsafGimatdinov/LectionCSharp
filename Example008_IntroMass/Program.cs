883f8c00c9523ab80f88101a829b08db420a04e7
InsafGimatdinov <insaf.gimatdinov@mail.ru>
2021-12-29 20:25:54 +0300

    Add All Example 008 - 011

---
+++ Example008_IntroMass/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+*/
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 50000;
+int b1 = 108;
+int c1 = 4;
+int a2 = 8;
+int b2 = 1800;
+int c2 = 11;
+int a3 = 300;
+int b3 = 5;
+int c3 = 16600;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+ 
+Console.WriteLine("max = ");
+Console.WriteLine(max);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+*/
+
+int[] array = { 1, 56, 3, 76, 3, 7, 2, 6, 3 };
+
+int n = array.Length;
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
